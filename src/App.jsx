d0858f0998ac10e936c9d261238470d2fc1229b4
AnshulKumar <134647111+AnshulKumar72@users.noreply.github.com>
2025-04-19 23:05:17 +0530

    Done !

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import React, { Suspense, lazy } from "react";
+import Navbar from "./components/NavBar";
+import Contact from "./sections/Contact";
+import Experience from "./sections/Experience";
+import FeatureCards from "./sections/FeatureCards";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import LogoShowcase from "./sections/LogoShowcase";
+import ShowcaseSection from "./sections/ShowcaseSection";
+import TechStack from "./sections/TechStack";
+import Testimonials from "./sections/Testimonials";
 
-// Lazy load sections and components
-const Navbar = lazy(() => import("./components/NavBar"));
-const Hero = lazy(() => import("./sections/Hero"));
-const ShowcaseSection = lazy(() => import("./sections/ShowcaseSection"));
-const LogoShowcase = lazy(() => import("./sections/LogoShowcase"));
-const FeatureCards = lazy(() => import("./sections/FeatureCards"));
-const Experience = lazy(() => import("./sections/Experience"));
-const TechStack = lazy(() => import("./sections/TechStack"));
-const Testimonials = lazy(() => import("./sections/Testimonials"));
-const Contact = lazy(() => import("./sections/Contact"));
-const Footer = lazy(() => import("./sections/Footer"));
 
 const App = () => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <>
     <Navbar />
     <Hero />
     <ShowcaseSection />
@@ -24,7 +22,7 @@ const App = () => (
     <Testimonials />
     <Contact />
     <Footer />
-  </Suspense>
+  </>
 );
 
 export default App;
